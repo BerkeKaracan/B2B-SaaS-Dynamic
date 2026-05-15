7291b286cefc4f4c5567a555f36da9fff16cd9c2
BerkeKaracan <berkekaracan1113@gmail.com>
2026-05-15 16:17:57 +0300

    general: organize to deploy to render

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 # Configure CORS to allow communication with the Next.js frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], 
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
